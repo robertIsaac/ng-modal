04919d3c584b821fb9317e1da0451ce1885a6e59
robertIsaac <dev.robert.isaac@gmail.com>
2023-02-09 08:57:24 +0200

    add modal component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ModalComponent,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
